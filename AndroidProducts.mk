49e76e2c85b861eed273ad039e0b8ccfaf0bc5c4
Koushik Dey <123065250+Koushikdey2003@users.noreply.github.com>
2023-03-20 15:49:25 +0000

    RMX2020: crDroid Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_RMX2020.mk
+    $(LOCAL_DIR)/lineage_RMX2020.mk
--- lineage_RMX2020.mk
+++ lineage_RMX2020.mk
@@ -12,14 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common EvolutionX stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
-TARGET_USES_MINI_GAPPS := true
-TARGET_SUPPORTS_QUICK_TAP := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := evolution_RMX2020
+PRODUCT_NAME := lineage_RMX2020
 PRODUCT_DEVICE := RMX2020
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2020
