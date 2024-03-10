d0b8c34f3389c3a8e06e9c7eeac37594bc90ced8
Koushik Dey <kousickdey3@gmail.com>
2024-03-10 15:22:32 +0530

    RMX2020: Compile kernel with AOSP Clang v18.0.0
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,9 +13,7 @@ rm -rf hardware/mediatek
 git clone https://github.com/ArrowOS/android_hardware_mediatek -b arrow-14.0 hardware/mediatek
 
 # Mediatek Sepolicy_vndr
-rm -rf device/mediatek/sepolicy_vndr
 git clone https://github.com/ArrowOS/android_device_mediatek_sepolicy_vndr -b arrow-14.0 device/mediatek/sepolicy_vndr
 
-# AOSP 17.0.4 clang-r498229b
-rm -rf prebuilts/clang/host/linux-x86/clang-r498229b
-git clone --depth=1 https://gitlab.com/Koushikdey2003/android_prebuilts_clang_host_linux-x86_clang-r498229b.git prebuilts/clang/host/linux-x86/clang-r498229b
+# AOSP 18.0.0 clang-r510928
+git clone --depth=1 https://gitlab.com/Koushikdey2003/prebuilts_clang_host_linux-x86_clang-r510928.git
